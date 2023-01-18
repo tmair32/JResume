fc23b50388c062be4bf4f719b93b8e79b65a6908
tmair32 <tmair32@gmail.com>
2023-01-18 11:07:13 +0900

    🔧 Setting Vite Resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
@@ -5,4 +6,9 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [eslintPlugin(), vue()],
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+    },
+  },
 })
